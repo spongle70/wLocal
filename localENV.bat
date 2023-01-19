1065234f13fed9b17897104bdbb96db0d5eeb609
USER <USER@DOMAIN.com>
2023-01-19 23:00:53 +0000

    .

--- localENV.bat
+++ localENV.bat
@@ -2,11 +2,11 @@
 set check_computername=%COMPUTERNAME%
 echo "Found computer %check_computername%"
 
-set PACKAGES="C:\Packages"
-set WPUBLIC="C:\wPublic"
+set PACKAGES=C:\Packages
+set WPUBLIC=C:\wPublic
 
-set BASE="C:\Scripts\wBase"
-set LOCAL="C:\Scripts\wLocal"
-set TMESSAGE="%BASE%\Msg\tmessage.bat"
-set TMESSAGEH="%BASE%\Msg\tmessageH.bat"
-set POWERSHELL="%SYSTEMROOT%\system32\WindowsPowerShell\v1.0\powershell.exe"
+set BASE=C:\Scripts\wBase
+set LOCAL=C:\Scripts\wLocal
+set TMESSAGE=%BASE%\Msg\tmessage.bat
+set TMESSAGEH=%BASE%\Msg\tmessageH.bat
+set POWERSHELL=%SYSTEMROOT%\system32\WindowsPowerShell\v1.0\powershell.exe
