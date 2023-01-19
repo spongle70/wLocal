fc98d5f269dec25f1e25424acce089d5e2dfc6c3
USER <USER@DOMAIN.com>
2023-01-19 16:12:37 +0000

    .

--- archive/mybackup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set myname=%COMPUTERNAME%
-echo "Found computer %myname%"
-
-call C:\Scripts\Base\tmessage.bat '"%myname% backup starting"'
-"C:\Program Files (x86)\Software of Excellence\EXACT\backup.exe"
-date /t > c:\finished.txt
-call C:\Scripts\Base\tmessage.bat '"FINISHED %myname% backup "'
