4332b7a13ce437d0e25d41f53505332b1720105e
USER <USER@DOMAIN.com>
2023-02-26 21:31:03 +0000

    .

--- PS/ShowSeconds.ps1
+++ PS/ShowSeconds.ps1
@@ -1,1 +1,1 @@
-Remove-Item -Path 'HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Run\com.squirrel.Teams.Teams'
+Remove-ItemProperty -Path 'HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'com.squirrel.Teams.Teams'
